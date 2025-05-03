18755364de81593edea0051b3a50252aa0c00191
PedroBettencourt <pedropbettencourt@gmail.com>
2025-05-03 18:42:33 +0000

    Fix lint error

--- src/tests/Shop.test.jsx
+++ src/tests/Shop.test.jsx
@@ -1,4 +1,4 @@
-import { vi, describe, it, expect } from "vitest";
+import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import Shop from "../Shop/Shop";
